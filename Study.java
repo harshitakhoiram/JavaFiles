4f7e9d2fa8c25d16fde7020cdea75289dccca3c4
harshitakhoiram <harshitakhoiram@gmail.com>
2024-08-18 18:11:29 +0530

    p

--- Study.java
+++ Study.java
@@ -1,6 +1,11 @@
 import java.io.*;
 public class Study {
+    static int m = 100; //static variable
     public static void main(String[] args){
+        int data = 80; //intance variable
         System.out.println("\nHello World!\n");
+        System.out.println("JVM is an abstract machine that doesn't exists physicaly but provides a runtime environment in which Java bytecode can be excevuted ");
+        System.out.println(m);
+        System.out.println(data);
     }
 }
